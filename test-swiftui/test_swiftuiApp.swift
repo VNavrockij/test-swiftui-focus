e2f33b6b69a352c6b23632327194d807f87c8b4e
Vitalii Navrotskyi <vitalik.nav@gmail.com>
2026-03-13 21:14:36 +0200

    Initial Commit

---
+++ test-swiftui/test_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_swiftuiApp.swift
+//  test-swiftui
+//
+//  Created by Vitalii Navrotskyi on 13.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct test_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
